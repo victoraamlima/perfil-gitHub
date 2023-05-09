62f516d96c36271762abc38bd2bb2106249ceb10
Victor Lima <victoraamlima@gmail.com>
2023-05-09 20:51:53 -0300

    Add validation

--- src/js/index.js
+++ src/js/index.js
@@ -6,46 +6,43 @@ import { user } from "./objects/user.js"
 import { screen } from "./objects/screen.js"
 
 document.getElementById("btn-search").addEventListener("click", () => {
-    const userName = document.getElementById("input-search").value
-    if(validateEmptyInput(userName)) return
-    getUserData(userName)
+  const userName = document.getElementById("input-search").value
+  if (validateEmptyInput(userName)) return
+  getUserData(userName)
 })
 
 document.getElementById("input-search").addEventListener("keyup", (e) => {
-    const userName = e.target.value
-    const key = e.which || e.keyCode
-    const isEnterKeyPressed = key === 13
+  const userName = e.target.value
+  const key = e.which || e.keyCode
+  const isEnterKeyPressed = key === 13
 
-    if(isEnterKeyPressed) {
-    validateEmptyInput(userName)
+  if (isEnterKeyPressed) {
+    if (validateEmptyInput(userName)) return
     getUserData(userName)
-    }
+  }
 })
 
 function validateEmptyInput(userName) {
-    if (userName.length === 0) {
-        alert("Write the username")
-        return true
-    }
+  if (userName.length === 0) {
+    alert("Write the username")
+    return true
+  }
 }
 
 async function getUserData(userName) {
-    const userResponse = await getUser(userName)
-    if (userResponse.message === "Not Found") {
-        screen.renderNotFound()
-        return
-    }
+  const userResponse = await getUser(userName)
+  if (userResponse.message === "Not Found") {
+    screen.renderNotFound()
+    return
+  }
 
-    const repositoriesResponse = await getRepositories(userName)
+  const repositoriesResponse = await getRepositories(userName)
 
-    const eventsResponse = await getEvents(userName)
+  const eventsResponse = await getEvents(userName)
 
-    user.setInfo(userResponse)
-    user.setRepositories(repositoriesResponse)
-    user.setEvents(eventsResponse)
+  user.setInfo(userResponse)
+  user.setRepositories(repositoriesResponse)
+  user.setEvents(eventsResponse)
 
-    screen.renderUser(user)
+  screen.renderUser(user)
 }
-
-
-getUserData("victoraamlima")
