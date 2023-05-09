2aa0294ac4583cce0a3a9054fb29ca6a34931ef9
Victor Lima <victoraamlima@gmail.com>
2023-05-09 20:32:59 -0300

    refactoring

--- src/js/services/repositories.js
+++ src/js/services/repositories.js
@@ -1,19 +1,8 @@
 import { baseUrl } from "../variables.js"
 
 async function getRepositories(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/repos`)
-
-    // console.log(await responseRepositories.json())
-    // const response = await fetch(`https://api.github.com/repos/${userName}/product-preview-card-component-main/languages`)
-
-    return await response.json()
+  const response = await fetch(`${baseUrl}/${userName}/repos`)
+  return await response.json()
 }
-
-// async function getRepositoriesLanguages(userName) {
-//     // console.log
-//     const response = await fetch(`https://api.github.com/repos/${userName}/product-preview-card-component-main/languages`)
-//     return await response.json()
-// }
-
 
 export { getRepositories }
